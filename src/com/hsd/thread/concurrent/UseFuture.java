30452614ef2659c7adcd3c641dd833cbd1b5f942
huangshengda <huangshengda@codyy.com>
2017-06-10 13:09:24 +0800

    添加disruptor

--- src/com/hsd/thread/concurrent/UseFuture.java
+++ src/com/hsd/thread/concurrent/UseFuture.java
@@ -3,6 +3,7 @@ package com.hsd.thread.concurrent;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 import java.util.concurrent.FutureTask;
 
 public class UseFuture implements Callable<String> {
@@ -31,17 +32,19 @@ public class UseFuture implements Callable<String> {
 		// 创建一个固定线程的线程池且线程数为1,
 		ExecutorService executor = Executors.newFixedThreadPool(1);
 		// 这里提交任务future,则开启线程执行RealData的call()方法执行
-		executor.submit(future);
+		Future<?> submit = executor.submit(future);
+
+		System.out.println(submit.get());
 		System.out.println("请求完毕");
 		try {
 			// 这里可以做额外的数据操作，也就是主程序执行其他业务逻辑
-			Thread.sleep(2000);
+			Thread.sleep(1000);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		// 调用获取数据方法,如果call()方法没有执行完成,则依然会进行等待
-		System.out.println("数据：" + future.get());
 		executor.shutdown();
+		System.out.println("数据：" + future.get());
 	}
 
 }
