f8bcadcab9c67cfe2e4502b85b4278c08feb470b
huangshengda <huangshengda@codyy.com>
2017-09-05 10:05:01 +0800

    add

---
+++ src/com/hsd/zookeeper/curator/cluster/Client2.java
@@ -0,0 +1,10 @@
+package com.hsd.zookeeper.curator.cluster;
+
+public class Client2 {
+
+	public static void main(String[] args) throws Exception {
+
+		CuratorWatcher watcher = new CuratorWatcher();
+		Thread.sleep(100000000);
+	}
+}
---
+++ src/com/hsd/zookeeper/watcher/impl/Security.java
@@ -0,0 +1,16 @@
+package com.hsd.zookeeper.watcher.impl;
+
+import com.hsd.zookeeper.watcher.i.Watcher;
+
+/**  
+ * @Description: TODO(这里用一句话描述这个类的作用)  
+ * @author huangshengda  
+ * @date 2017年8月30日   
+ *  
+*/
+public class Security implements Watcher {
+	@Override
+	public void update() {
+		System.out.println("运输车有行动，保安贴身保护");
+	}
+}
